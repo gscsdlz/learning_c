5ee41c50814b1c658a5988aa2814558c944420f7
gscsdlz <lz842063523@foxmail.com>
2017-09-05 18:27:42 +0800

    设置基本函数

---
+++ Include/ProblemController.php
@@ -0,0 +1,30 @@
+<?php
+
+class ProblemController extends Smarty
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 新增一个题目
+     */
+    public function add_problem() {
+
+    }
+
+    /**
+     * 删除指定题目
+     */
+    public function del_problem() {
+
+    }
+
+    /**
+     * 修改指定题目
+     */
+    public function modify_problem() {
+
+    }
+}
