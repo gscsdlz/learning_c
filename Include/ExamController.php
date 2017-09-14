a88efa2393671d8d596d5446a2295b3a4d4de49f
gscsdlz <lz842063523@foxmail.com>
2017-09-14 17:21:45 +0800

    UPDATE

--- Include/ExamController.php
+++ Include/ExamController.php
@@ -10,11 +10,14 @@ class ExamController extends Smarty
 {
     private $problemModel = null;
     private $examModel = null;
+    private $chapterModel = null;
+
     public function __construct()
     {
         parent::__construct();
         $this->problemModel = new ProblemModel();
         $this->examModel = new ExamModel();
+        $this->chapterModel = new ChapterModel();
     }
 
     public function show()  {
@@ -23,18 +26,22 @@ class ExamController extends Smarty
             parent::display('login.html');
             return;
         }
-        $log_id = get('id', null);
+        $sectionID = get('id');
+        $log_id = get('pid', null);
         $sig = false;
         if(is_null($log_id)) {
-            $sectionID = 9;
             $pro_ids = $this->problemModel->get_pro_id($sectionID, 1);
             $sig = true;
         } else {
             $pro_ids = $this->examModel->get_problem($log_id);
         }
         $ids = array();
-        foreach ($pro_ids as $row)
+        $k = 1;
+        foreach ($pro_ids as $row) {
             $ids[] = (int)$row[0];
+            if($k++ == 10)
+                break;
+        }
         unset($pro_ids);
         if ($sig == true) {
             shuffle($ids);
@@ -49,9 +56,15 @@ class ExamController extends Smarty
             $k++;
         }
         if ($sig == true)
-            header('Location:/learning_c/exam/show/'.$log_id);
+            header('Location:/learning_c/exam/show/'.$sectionID.'/'.$log_id);
 
         parent::assign('lists', $res);
         parent::display('problem.html');
+    }
+
+    public function index()
+    {
+        parent::assign('lists', $this->chapterModel->get_all_chapter());
+        parent::display('chapter_choose.html');
     }
 }
