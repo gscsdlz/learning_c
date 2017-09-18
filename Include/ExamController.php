47cddae82ac6678c4ce5c328ac635411641078f2
gscsdlz <lz842063523@foxmail.com>
2017-09-18 10:47:57 +0800

    完成基本的考试模块

--- Include/ExamController.php
+++ Include/ExamController.php
@@ -30,7 +30,7 @@ class ExamController extends Smarty
         $log_id = get('pid', null);
         $sig = false;
         if(is_null($log_id)) {
-            $pro_ids = $this->problemModel->get_pro_id($sectionID, 1);
+            $pro_ids = $this->problemModel->get_pro_id($sectionID, 0);
             $sig = true;
         } else {
             $pro_ids = $this->examModel->get_problem($log_id);
@@ -45,7 +45,7 @@ class ExamController extends Smarty
         unset($pro_ids);
         if ($sig == true) {
             shuffle($ids);
-            $log_id = $this->examModel->init(1, $ids);
+            $log_id = $this->examModel->init($_SESSION['stu_id'], $ids, $sectionID);
         }
 
         $res = array();
@@ -58,6 +58,7 @@ class ExamController extends Smarty
         if ($sig == true)
             header('Location:/learning_c/exam/show/'.$sectionID.'/'.$log_id);
 
+        parent::assign('log_id', $log_id);
         parent::assign('lists', $res);
         parent::display('problem.html');
     }
@@ -66,5 +67,28 @@ class ExamController extends Smarty
     {
         parent::assign('lists', $this->chapterModel->get_all_chapter());
         parent::display('chapter_choose.html');
+    }
+
+    public function answer()
+    {
+        $options = post('options');
+        $pros = post('pros');
+        $log_id = post('log_id');
+        $res = array_combine($pros, $options);
+        $this->examModel->save($log_id, $res);
+        echo json_encode([
+            'status' => true
+        ]);
+    }
+
+    public function result() {
+        $log_id = get('id');
+        $res = $this->examModel->get_result($log_id);
+        parent::assign('stime', $res[2][0]);
+        parent::assign('etime', $res[2][1]);
+        parent::assign('chapterName', $res[2][2]);
+        parent::assign('pro_info', $res[0]);
+        parent::assign('pro_option', $res[1]);
+        parent::display('exam_result.html');
     }
 }
--- Model/ExamModel.php
+++ Model/ExamModel.php
@@ -13,10 +13,10 @@ class ExamModel extends DB
         parent::__construct();
     }
 
-    public function init($user_id, $ids)
+    public function init($user_id, $ids, $chapterID)
     {
-        $log_id = parent::query("INSERT INTO pro_log (stu_id, stime, etime) VALUES (?, ?, 0)",
-            [$user_id, time()]);
+        $log_id = parent::query("INSERT INTO pro_log (stu_id, stime, etime, sec_cha_id) VALUES (?, ?, 0, ?)",
+            [$user_id, time(), $chapterID]);
         foreach ($ids as $pro_id) {
             parent::query("INSERT INTO pro_result (log_id, pro_id, option_id) VALUES (?,?,?)",
                 [$log_id, $pro_id, 0]);
@@ -28,5 +28,25 @@ class ExamModel extends DB
     {
         return parent::query_fetch_all("SELECT pro_id FROM pro_result WHERE log_id = ?",
             [$log_id]);
+    }
+
+    public function save($log_id, $res) {
+
+        foreach($res as  $k => $v)
+            parent::query("UPDATE pro_result SET option_id = ? WHERE pro_id = ? AND log_id = ?",
+                [$v, $k, $log_id]);
+        parent::query("UPDATE pro_log SET etime = ? WHERE log_id = ?",
+            [time(), $log_id]);
+    }
+
+    public function get_result($log_id) {
+        $res1 = parent::query_fetch_all("SELECT pro_result.pro_id, log_id, pro_result.option_id, pro_info, problem.option_id FROM `pro_result` LEFT JOIN problem USING (pro_id) WHERE log_id = ?",
+            [$log_id]);
+        foreach($res1 as $row) {
+            $res2[] = parent::query_fetch_all("SELECT * FROM pro_option WHERE pro_id = ?", [$row[0]]);
+        }
+        $res3 = parent::query_one("SELECT stime, etime,chapter_name FROM pro_log LEFT JOIN chapter USING (sec_cha_id) WHERE log_id = ?",
+            [$log_id]);
+        return [$res1, $res2, $res3];
     }
 }
